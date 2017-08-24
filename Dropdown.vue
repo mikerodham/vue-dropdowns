16aa19c842f7c03b6e65f581415c7041aa995a43
Mike Rodham <switchytm@gmail.com>
2017-08-24 23:00:54 +0100

    Initial commit

---
+++ Dropdown.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="btn-group">
+        <a href="javascript:void(0)" class="form-control dropdown-toggle" data-toggle="dropdown" aria-expanded="false"  v-if="selectedOption !== null">
+            {{ selectedOption.name }}
+            <span class="caret"></span>
+            <div class="ripple-container"></div>
+        </a>
+
+        <a href="javascript:void(0)" class="form-control dropdown-toggle" data-toggle="dropdown" aria-expanded="false" v-if="selectedOption === null">
+            Please Select an Item
+            <span class="caret"></span>
+            <div class="ripple-container"></div>
+        </a>
+        <ul class="dropdown-menu">
+            <li v-for="option in options">
+                <a href="javascript:void(0)" @click="updateOption(option)">
+                    {{ option.name }}
+                </a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                selectedOption: {},
+            }
+        },
+        props: {
+            options: {
+                type: [Array, Object]
+            },
+            selected: {},
+        },
+
+        mounted() {
+            this.selectedOption = this.selected;
+        },
+
+        methods: {
+            updateOption(option) {
+                this.selectedOption = option;
+                this.$emit('updateOption', this.selectedOption);
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .btn-group {
+        width: 100%;
+        a {
+            &:hover {
+                text-decoration: none;
+            }
+        }
+    }
+
+    .dropdown-toggle {
+        color: #636b6f;
+        text-transform: none;
+        font-weight: 300;
+    }
+
+    .dropdown-menu > li > a {
+        padding: 10px 30px;
+
+        &:hover {
+            background: #efefef;
+            color: #409FCB;
+        }
+    }
+
+    .caret {
+        float: right;
+    }
+</style>
---
+++ readme.md
@@ -0,0 +1,14 @@
+# vue-dropdown
+A prettier way to display select boxes.
+
+# Requirements
+
+- [Vue.js](https://github.com/yyx990803/vue) `^2.0.0`
+
+# Installation
+
+- To yet be uploaded to NPM
+
+# License
+
+[The MIT License](http://opensource.org/licenses/MIT)
